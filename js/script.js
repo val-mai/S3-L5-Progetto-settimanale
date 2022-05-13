d70a2c3a257bdd70f35fd7651a7cd9e7f7229256
val-mai <valerio.maiolini@gmail.com>
2022-05-13 16:58:14 +0200

    multicount

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let prev;
 let now;
 let operand;
+let evaluated;
 
 function appendNum(number) {
     if (number === '.' && document.getElementById('now').value.includes('.')) {
@@ -19,29 +20,44 @@ function del() {
 }
 
 function operation(operatore) {
-    operand = operatore;
-    prev = parseFloat(document.getElementById('now').value);
-    document.getElementById('prev').value = document.getElementById('prev').value + document.getElementById('now').value + ' ' + operatore + ' ';
-    document.getElementById('now').value = "";
-}
-
-function eval() {
-    let evaluated;
-    now = parseFloat(document.getElementById('now').value);
-    document.getElementById('prev').value = document.getElementById('prev').value + document.getElementById('now').value;
-    if (operand == '+') {
-        evaluated = prev + now;
-        document.getElementById('now').value = evaluated;
-    } else if (operand == '-') {
-        evaluated = prev - now;
-        document.getElementById('now').value = evaluated;
-    } else if (operand == '/') {
-        evaluated = prev / now;
-        document.getElementById('now').value = evaluated;
-    } else if (operand == '*') {
-        evaluated = prev * now;
-        document.getElementById('now').value = evaluated;
+    if (document.getElementById('prev').value === "" && document.getElementById('now').value === "") {
+        return
+    } else if (document.getElementById('prev').value !== "" && document.getElementById('now').value !== "") {
+        eval();
+        operand = operatore;
+        document.getElementById('prev').value = document.getElementById('now').value + ' ' + operatore + ' ';
+        document.getElementById('now').value = "";
+        prev = evaluated;
+    } else {
+        operand = operatore;
+        prev = parseFloat(document.getElementById('now').value);
+        document.getElementById('prev').value = document.getElementById('prev').value + document.getElementById('now').value + ' ' + operatore + ' ';
+        document.getElementById('now').value = "";
     }
 }
 
+function eval() {
+    if (document.getElementById('prev').value != "") {
+        now = parseFloat(document.getElementById('now').value);
+        document.getElementById('prev').value = document.getElementById('prev').value + document.getElementById('now').value;
+        if (operand == '+') {
+            evaluated = prev + now;
+            document.getElementById('now').value = evaluated;
+        } else if (operand == '-') {
+            evaluated = prev - now;
+            document.getElementById('now').value = evaluated;
+        } else if (operand == '/') {
+            evaluated = prev / now;
+            document.getElementById('now').value = evaluated;
+        } else if (operand == '*') {
+            evaluated = prev * now;
+            document.getElementById('now').value = evaluated;
+        }
+    } else {
+        return
+    }
+}
 
+function changeSign() {
+    document.getElementById('now').value = document.getElementById('now').value * -1;
+}
